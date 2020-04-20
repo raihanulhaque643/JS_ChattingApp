42b8b4f8d2a07155455bcc7b7d4efb7bc58302a4
Raihanul Haque <raihanulhaque@rocketmail.com>
2020-04-20 17:37:25 +0600

    new chats can now be sent from DOM

--- scripts/app.js
+++ scripts/app.js
@@ -1,9 +1,24 @@
 //DOM queries:
 const chatList = document.querySelector('.chat-list');
+const newChatForm = document.querySelector('.new-chat');
 
 //class instances
 const chatUI = new ChatUI(chatList);
-const chatroom = new Chatroom('gaming', 'shaun');
+const chatroom = new Chatroom('general', 'shaun');
+
+//add a new chat
+newChatForm.addEventListener('submit', e => {
+    e.preventDefault();
+    const message = newChatForm.message.value.trim();
+    chatroom.addChat(message)
+        .then(() => {
+            newChatForm.reset()
+        })
+        .catch ((err) => {
+            console.log(err);
+        })
+
+});
 
 //get chats and render
 chatroom.getChats((data) => {

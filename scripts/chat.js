ca1a1f807d2dee86bbe14a05991144d55ba48491
Raihanul Haque <raihanulhaque@rocketmail.com>
2020-04-20 15:47:30 +0600

    real time listerner added

--- scripts/chat.js
+++ scripts/chat.js
@@ -25,11 +25,23 @@ class Chatroom {
           const response = await this.chats.add(chat);
           return response;
     }
+    //set up real time listener:
+    getChats(callback){
+        this.chats
+            .onSnapshot(snapshot => {
+                snapshot.docChanges().forEach(change => {
+                    if(change.type === 'added'){
+                        //update the UI
+                        callback(change.doc.data());
+                    }
+                });
+            });
+    }
 }
 
 //create new chatroom:
 const chatroom = new Chatroom('gaming', 'shaun');
 // console.log(chatroom); 
-chatroom.addChat('hello everyone')
-    .then(() => console.log('chat added'))
-    .catch(err => console.log(err));
+chatroom.getChats((data) => {
+    console.log(data);
+})

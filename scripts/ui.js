aa7964f478b4d40a451f73a5c40b983ffd64cc75
Raihanul Haque <raihanulhaque@rocketmail.com>
2020-04-20 17:17:44 +0600

    DateFns libary included and used to make the dates on the DOM look better

--- scripts/ui.js
+++ scripts/ui.js
@@ -4,17 +4,20 @@
 
 class ChatUI {
     constructor(list){
-        this.list = list;
+      this.list = list;
     }
     render(data){
-        const html = `
-            <li class="list-group-item">
-                <span class="username">${data.username}</span>
-                <span class="message">${data.message}</span>
-                <div class="time">${data.created_at.toDate()}</div>
-            </li>
-        `;
-
-        this.list.innerHTML += html;
+      const when = dateFns.distanceInWordsToNow(
+        data.created_at.toDate(),
+        { addSuffix:true }
+      );
+      const html = `
+        <li class="list-group-item">
+          <span class="username">${data.username}</span>
+          <span class="message">${data.message}</span>
+          <div class="time">${when}</span>
+        </li>
+      `;
+      this.list.innerHTML += html;
     }
-}
+  }
